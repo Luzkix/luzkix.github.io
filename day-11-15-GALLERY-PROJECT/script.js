fcda00142d20ebb72ea948c60144cf6aef11cb70
ZdenekL <luzkix@seznam.cz>
2020-06-29 15:31:20 +0200

    xwe

---
+++ day-11-15-GALLERY-PROJECT/script.js
@@ -0,0 +1,133 @@
+let photo1 = {
+    photo: "gallery/1_Erice.jpg",
+    title: "Erice",
+    description: "(2019) Erice is a very small town located on top of Mount Erice, at around 750 metres (2,460 ft) above sea level, overlooking the city of Trapani in Sicily. It is beutiful and breathtaking especially when flooded by the clouds."
+};
+
+let photo2 = {
+    photo: "gallery/2_arancini.jpg",
+    title: "Arancini",
+    description: "(2019) Arancini are Italian snacks consisting of a ball of rice coated with bread crumbs and then deep fried—a staple of Sicilian cuisine. A number of regional variants exist which differ in fillings and shape. And most important, it is soo tasty if properly made! Definitely must have if visiting Sicily."
+};
+
+let photo3 = {
+    photo: "gallery/3_panama.jpg",
+    title: "View of Panama City",
+    description: "(2018) On the way to Cuba we made a one-day break in Panama City. Panama is famous for its Panama Canal and also as a tax heaven. We took a taxi from the airport and look what a wonderful scenery did we witnessed!"
+};
+
+let photo4 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo5 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo6 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo7 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo8 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo9 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo10 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo11 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo12 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo13 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo14 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo15 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo16 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo17 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo18 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo19 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+let photo20 = {
+    photo: "gallery/4_havana.jpg",
+    title: "Havana, Cuba",
+    description: "(2018) Havana, with its narrow streets and overhanging balconies, is the traditional center of Cubans commerce, industry, and entertainment, as well as being a residential area. We loved Havana´s nice people, mojito and of course old American cars (taxis) which put you back to the 50´s. And of course, the sunset…"
+};
+
+
+
+
+
+
+let imagesData = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9,
+    photo10, photo11, photo12, photo13, photo14, photo15, photo16, photo17, photo18, photo19, photo20];
+
+let currentPhoto = 0;
+
+$('#photo').attr('src', imagesData[currentPhoto].photo);
+$('#photo-title').text(imagesData[currentPhoto].title);
+$('#photo-description').text(imagesData[currentPhoto].description);
